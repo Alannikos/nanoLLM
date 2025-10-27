5b71f284436d9ce73b9ee78e3a91b0adb1386b40
alannikos <alannikos768@outlook.com>
2025-10-27 11:46:21 +0800

    implement the attention, positionwisefeedforward, and the add&norm

--- 1_transformer/model.py
+++ 1_transformer/model.py
@@ -10,7 +10,35 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
 from .dataset import MTDataset
-# from .utils import *
+from .utils import get_attn_pad_mask, get_subsequent_mask
+from .utils import *
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, embedding_dim: int, eps: float = 1e-8):
+        super(LayerNorm, self).__init__()
+        self.gamma = nn.Parameter(torch.ones(embedding_dim))
+        self.beta = nn.Parameter(torch.zeros(embedding_dim))
+
+        self.eps = eps
+    
+    def forward(self, x: torch.Tensor):
+        """进行norm的前向过程
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            形状一般是(batch_size, seq_len, embedding_dim)
+        """
+
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True)
+
+        out = (x - mean) / torch.sqrt(var + self.eps)
+        out = self.gamma * out + self.beta
+
+        return out
+
 
 # 这里有的继承nn.Embedding?
 class TokenEmbedding(nn.Module):
@@ -101,6 +129,229 @@ class PositionalEncoding(nn.Module):
         # 加dropout: 随机丢弃一部分神经元的输出，防止模型过拟合。
         return self.dropout(x)
 
+
+class ScaleDotProductAttention(nn.Module):
+    def __init__(self, d_k: int):
+        super(ScaleDotProductAttention, self).__init__()
+        self.d_k = d_k
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor, mask: torch.Tensor):
+        """点积注意力的实现
+
+        Parameters
+        ----------
+        query : torch.Tensor
+            经过Q矩阵的变换的input, shape=(batch_size, n_head, seq_len, d_k)
+        key : torch.Tensor
+            经过K矩阵的变换的input, shape=(batch_size, n_head, seq_len, d_k)
+        value : torch.Tensor
+            经过V矩阵的变换的input, shape=(batch_size, n_head, seq_len, d_k)
+        mask : torch.Tensor
+            subsequent_mask
+        """
+
+        # shape = (batch_size, n_head, seq_len, seq_len)
+        attention_wieght = torch.matmul(query, key.transpose(-1, -2)) / math.sqrt(self.d_k)
+
+        if mask is not None:
+            # !!!
+            # 这里写1e-9好像会有问题，之后再改
+            attention_wieght = attention_wieght.masked_fill(mask, -1e9)
+
+        attention_wieght = self.softmax(attention_wieght)
+
+        # (batch_size, n_head, seq_len, d_k)
+        # 这个embedding_dim要看实际上分了多少个head
+        attention_score = torch.matmul(attention_wieght, value)
+
+        return attention_score, attention_wieght
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_head: int, embedding_dim: int):
+        self.n_head = n_head
+        self.embedding_dim = embedding_dim
+
+        assert embedding_dim % n_head == 0, "head的数量必须是embedding_dim的因子"
+
+        # d_k就是各个head里面embedding_dim的大小
+        self.d_k = embedding_dim // n_head
+        
+        # 下面就是attention, layernorm; 残差连接会体现在forward函数里
+        self.Wq = nn.Linear(embedding_dim, embedding_dim, bias=False)
+        self.Wk = nn.Linear(embedding_dim, embedding_dim, bias=False)
+        self.Wv = nn.Linear(embedding_dim, embedding_dim, bias=False)
+
+        self.attention = ScaleDotProductAttention(self.d_k)
+
+        self.Wo = nn.Linear(embedding_dim, embedding_dim, bias=False)
+
+    def forward(self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor, mask: torch.Tensor):
+        # 注意，其实现在输入的q, k, v应该都是x
+
+        # 流程：线性变化->划分多个head->attention->concat 
+
+        batch_size = query.size(0)
+        q, k, v = self.Wq(query), self.Wk(key), self.Wv(value)
+
+        # (batch_size, seq_len, embedding_dim) -> \
+        #        (batch_size, seq_len, n_head, d_k)
+        q = q.reshape(batch_size, -1, self.n_head, self.d_k)
+        k = k.reshape(batch_size, -1, self.n_head, self.d_k)
+        v = v.reshape(batch_size, -1, self.n_head, self.d_k)
+
+        # 进行转置方便注意力计算
+        # (batch_size, seq_len, n_head, d_k) ->
+        #     (batch_size, n_head, seq_len, d_k)
+        q = q.transpose(1, 2)
+        k = k.transpose(1, 2)
+        v = v.transpose(1, 2)
+
+        if mask is not None:
+            mask = mask.unsqueeze(1).repeat(1, self.n_head, 1, 1)
+        
+        # (batch_size, n_head, seq_len, d_k)
+        attention_output, attention_weights = self.attention(q, k, v, mask)
+        # (batch_size, seq_len, n_head, d_k)
+        attention_output = attention_output.transpose(1, 2)
+        # (batch_size, seq_len, embedding_dim)
+        attention_output = attention_output.reshape(batch_size, -1, self.embedding_dim)
+
+        output = self.Wo(attention_output)
+
+        return output, attention_weights
+
+
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, embedding_dim: int, hidden_dim: int):
+        """前馈层，两个linear层加一个Relu
+
+        Parameters
+        ----------
+        embedding_dim : int
+        hidden_dim : int
+        dropout : float, optional
+            dropout, by default 0.1
+        """
+
+        super(PositionWiseFeedForward, self).__init__()
+
+        self.fc = nn.Sequential(
+            nn.Linear(embedding_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, embedding_dim)
+        )
+
+    def forwrd(self, x):
+
+        # shape = (batch_size, seq_len, embedding_dim)
+        out = self.fc(x)
+        return out
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, n_head: int, embedding_dim: int, hidden_dim: int, dropout: float):
+        self.n_head = n_head
+        self.embedding_dim = embedding_dim
+
+        self.multiHeadAttention = MultiHeadAttention(n_head=n_head, embedding_dim=embedding_dim)
+        self.norm1 = LayerNorm(embedding_dim=embedding_dim)
+        self.dropout1 = nn.Dropout(p=dropout)
+
+        self.positionWiseFeedForward = PositionWiseFeedForward(embedding_dim=embedding_dim, hidden_dim=hidden_dim)
+        self.norm2 = LayerNorm(embedding_dim=embedding_dim)
+        self.dropout2 = nn.Dropout(p=dropout)
+
+    def forward(self, x: torch.Tensor, mask: torch.Tensor = None):
+        """一个encoder的前向传播
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            每个encoderLayer的输入，shape=(batch_size, seq_len, embedding_dim)
+        mask : torch.Tensor
+            掩码
+        """
+
+        #========================================
+        #             Attention                 #
+        #========================================
+        residual = x
+        x = self.multiHeadAttention(x, x, x, mask)
+        x = self.dropout1(x)
+        # add & norm
+        x = self.norm1(x + residual)
+
+        #========================================
+        #                  FFN                  #
+        #========================================
+        residual = x
+        x = self.positionWiseFeedForward(x)
+        x = self.dropout2(x)
+        # add & norm
+        x = self.norm2(x + residual)
+
+        return x
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, n_head: int, embedding_dim: int, hidden_dim: int, dropout: float):
+        
+        self.maskMultiHeadAttention = MultiHeadAttention(n_head=n_head, embedding_dim=embedding_dim)
+        self.norm1 = LayerNorm(embedding_dim=embedding_dim)
+        self.dropout1 = nn.Dropout(p=dropout)
+
+        self.multiHeadAttention = MultiHeadAttention(n_head=n_head, embedding_dim=embedding_dim)
+        self.norm2 = LayerNorm(embedding_dim=embedding_dim)
+        self.dropout2 = nn.Dropout(p=dropout)
+
+        self.positionWiseFeedForward = PositionWiseFeedForward(embedding_dim=embedding_dim, hidden_dim=hidden_dim)
+        self.norm3 = LayerNorm(embedding_dim=embedding_dim)
+        self.dropout3 = nn.Dropout(p=dropout)
+
+    def forward(self, decoder_x: torch.Tensor, encoder_y: torch.Tensor, decoder_mask: torch.Tensor, encoder_mask: torch.Tensor):
+        """decoder的一个block的过程
+
+        Parameters
+        ----------
+        decoder_x : torch.Tensor
+            shape=(batch_size, target_len, embedding_dim)
+        encoder_y : torch.Tensor
+            shape=(batch_size, source_len, embedding_dim)
+        decoder_mask : torch.Tensor
+            shape=(batch_size, target_len, target_len)
+        encoder_mask : torch.Tensor
+            shape=(batch_size, target_len, source_len)
+        """
+
+        # step1: 计算自注意力分数
+        residual = decoder_x
+        x = self.maskMultiHeadAttention(decoder_x, decoder_x, decoder_x, decoder_mask)
+
+        # step2: add&norm
+        x = self.dropout1(x)
+        x = self.norm1(x + residual)
+
+        if encoder_y is not None:
+            # step3: 计算encoder-decoder attention分数
+            residual = encoder_y
+            x = self.multiHeadAttention(query=x, key=encoder_y, value=encoder_y, mask=encoder_mask)
+            
+            # step4: add&norm
+            x = self.dropout2(x)
+            x = self.norm2(x + residual)
+
+        # 5. fnn
+        residual = x
+
+        x = self.positionWiseFeedForward(x)
+        
+        # 6. add & norm
+        x = self.dropout3(x)
+        x = self.norm3(x + residual)
+
+        return x
 
 
 if __name__ == "__main__":
---
+++ 1_transformer/utils.py
@@ -0,0 +1,96 @@
+import os
+import sys
+import copy
+import math
+from typing import List
+
+import torch
+import torch.nn as nn
+import numpy as np
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+
+def get_attn_pad_mask(seq_q: torch.Tensor, seq_k: torch.Tensor):
+    """因为可能在数据中使用了padding, 不希望pad被加入到注意力中进行计算
+
+    Parameters
+    ----------
+    seq_q : torch.Tensor
+        (batch_size, len_q)
+    seq_k : torch.Tensor
+        (batch_size, len_k)
+    """
+
+    batch_sie, len_q = seq_q.size()
+    _, len_k = seq_k.size()
+
+    # (batch_size, 1, len_k)
+    pad_attn_mask = seq_k.eq(3).unsqueeze(1)
+
+    pad_attn_mask.expand(batch_sie, len_q, len_k)
+
+    return pad_attn_mask
+
+def get_subsequent_mask(seq: torch.Tensor):
+    """生成因果mask
+
+    Parameters
+    ----------
+    seq : torch.Tensor
+        输入的序列, (batch_size, target_len)
+    """
+
+    attn_shape = [seq.size(0), seq.size(1), seq.size(1)]
+
+    # torch.triu是留下了上三角矩阵，然后diagonal为1的话，则对角线的是0
+    # [[[0, 1, 1, 1],
+    # [0, 0, 1, 1],
+    # [0, 0, 0, 1],
+    # [0, 0, 0, 0]]]
+    subsequent_mask = torch.triu(torch.ones(attn_shape, dtype=torch.unit8), diagonal=1)
+
+    return subsequent_mask
+
+
+
+def aggreVocab(filePaths: List[str], outputPath: str):
+    """提取原始数据的词表，并且保存为字典文件
+
+    Parameters
+    ----------
+    filePaths : List[str]
+        输入文件位置
+    outputPath : str
+        保存文件的位置
+    """
+
+    # 存储所有的词汇
+    vocabList = []
+    for file in filePaths:
+        print(f"read {file}")
+        with open(file, "r") as f:
+            for line in f.readlines():
+                # 遍历每个词汇
+                words = line.strip().split(" ")
+                for word in words:
+                    vocabList.append(word)
+    
+    # vocabSet就是词表
+    vocabSet = set(vocabList)
+
+    # vocabDict存储{word:index}二元组
+    vocabDict = {'<START>': 0, '<END>': 1, 'UNK': 2, 'PAD': 3}
+    for index, word in enumerate(vocabSet):
+        vocabDict[word] = index + 4
+    
+    np.save(outputPath, vocabDict)
+
+def compute_bleu(translate, reference, references_lens):
+    pass
+
+
+
+if __name__ == "__main__":
+
+    # 把训练集，验证集和测试集的所有词汇都提取出来，防止OOD
+    aggreVocab(['../data/train.en.tok', '../data/valid.en.tok', '../data/test.en.tok'], '../data/vocab_en_freq_2.npy')
+    aggreVocab(['../data/train.zh.tok', '../data/valid.zh.tok', '../data/test.zh.tok'], '../data/vocab_zh_freq_2.npy')
